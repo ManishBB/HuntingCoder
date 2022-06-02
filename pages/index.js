dfb83658c027eda0295029116e07212c4d7503d2
Manish <mbb7224@gmail.com>
2022-06-02 23:07:20 +0530

    Tried CSS Components

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Script from 'next/script'
 import styles from '../styles/Home.module.css'
+import styles1 from '../styles/Home1.module.css'
+import styles2 from '../styles/Home2.module.css'
+
 
 export default function Home() {
   return (
@@ -14,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={styles.mainnav}>
+      <nav className={styles2.mainnav}>
         <ul>
           <Link href='/'><li>Home</li></Link>
           <Link href='/about'><li>About</li></Link>
