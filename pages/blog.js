dae0f586fec5894ed6c4fc12f2002b49c24a25d9
Manish <mbb7224@gmail.com>
2022-06-05 20:05:27 +0530

    Added blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
-    <div>This is Blog homepage</div>
+    <div className={styles.container}>
+        <main className={styles.main}>
+          <div className="blogItem">
+            <Link href={'/blogpost/learn-javascript'}>
+            <h3 className={styles.blogItemh3}>How to learn JS in 2022</h3></Link>
+            <p>JS is used to design logic in web!</p>
+          </div>
+
+          <div className="blogItem">
+            <h3>How to learn JS in 2022</h3>
+            <p>JS is used to design logic in web!</p>
+          </div>
+
+          <div className="blogItem">
+            <h3>How to learn JS in 2022</h3>
+            <p>JS is used to design logic in web!</p>
+          </div>
+
+          <div className="blogItem">
+            <h3>How to learn JS in 2022</h3>
+            <p>JS is used to design logic in web!</p>
+          </div>
+        </main>
+    </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import styles from '../../styles/BlogPost.module.css'
+
 
 const slug = () => {
 
@@ -7,7 +9,13 @@ const slug = () => {
     const {slug} = router.query
 
     return (
-        <div>{slug}</div>
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Title of the page {slug}</h1>
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio expedita nostrum dolorem aspernatur! Ducimus magnam veniam neque sunt necessitatibus quos cum sint minima repellendus ad maiores impedit obcaecati, quam deserunt in dolorum doloremque ex! A blanditiis hic voluptatum assumenda possimus aliquid eius ab et commodi, recusandae itaque temporibus velit eveniet!</p>
+            </main>
+        </div>
     )
 }
 
