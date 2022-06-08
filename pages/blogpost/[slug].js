60f2c9a112cc4398b8ecd8d3b212e75789400595
Manish <mbb7224@gmail.com>
2022-06-08 17:17:41 +0530

    Added slug api's

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,19 +1,29 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React , { useEffect , useState } from 'react'
 import styles from '../../styles/BlogPost.module.css'
 
 
 const slug = () => {
 
     const router = useRouter()
-    const {slug} = router.query
+    const [blog, setBlog] = useState()
+    useEffect(()=>{
+        if(!router.isReady) return;
+        const {slug} = router.query
+        fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a)=>{
+          return a.json()
+        }).then((parsed)=>{
+          console.log(parsed)
+          setBlog(parsed)
+        })
+      }, [router.isReady])
 
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h1>Title of the page {slug}</h1>
+                <h1>{blog && blog.title}</h1>
                 <hr />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio expedita nostrum dolorem aspernatur! Ducimus magnam veniam neque sunt necessitatibus quos cum sint minima repellendus ad maiores impedit obcaecati, quam deserunt in dolorum doloremque ex! A blanditiis hic voluptatum assumenda possimus aliquid eius ab et commodi, recusandae itaque temporibus velit eveniet!</p>
+                <p>{blog && blog.content}</p>
             </main>
         </div>
     )
