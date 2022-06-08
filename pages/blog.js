bdde6016c21e3a5976124c7a38784b70ed51361f
Manish <mbb7224@gmail.com>
2022-06-08 17:03:58 +0530

    Added Endpoints

--- pages/blog.js
+++ pages/blog.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useEffect , useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
 const Blog = () => {
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/api/blogs').then((a)=>{
+      return a.json()
+    }).then((parsed)=>{
+      console.log(parsed)
+      setBlogs(parsed)
+    })
+  }, [])
+
   return (
     <div className={styles.container}>
         <main className={styles.main}>
-          <div className="blogItem">
-            <Link href={'/blogpost/learn-javascript'}>
-            <h3 className={styles.blogItemh3}>How to learn JS in 2022</h3></Link>
-            <p>JS is used to design logic in web!</p>
-          </div>
 
-          <div className="blogItem">
-            <h3>How to learn JS in 2022</h3>
-            <p>JS is used to design logic in web!</p>
+          {blogs.map((blogItem)=>{
+            return <div className="blogItem" key={blogItem.title}>
+            <Link href={`/blogpost/${blogItem.slug}`}>
+            <h3 className={styles.blogItemh3}>{blogItem.title}</h3></Link>
+            <p>{blogItem.content.substr(0,140)}</p>
           </div>
+          })}
 
-          <div className="blogItem">
-            <h3>How to learn JS in 2022</h3>
-            <p>JS is used to design logic in web!</p>
-          </div>
 
-          <div className="blogItem">
-            <h3>How to learn JS in 2022</h3>
-            <p>JS is used to design logic in web!</p>
-          </div>
         </main>
     </div>
   )
