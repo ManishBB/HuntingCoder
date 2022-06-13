00df4519bec16de203d8eaa2310dfbddde4f08b2
Manish <mbb7224@gmail.com>
2022-06-13 23:55:47 +0530

    Added SSR

--- pages/blog.js
+++ pages/blog.js
@@ -2,18 +2,19 @@ import React, { useEffect , useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
-const Blog = () => {
+const Blog = (props) => {
 
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState(props.allBlogs)
+  console.log(blogs)
 
-  useEffect(()=>{
-    fetch('http://localhost:3000/api/blogs').then((a)=>{
-      return a.json()
-    }).then((parsed)=>{
-      console.log(parsed)
-      setBlogs(parsed)
-    })
-  }, [])
+  // useEffect(()=>{
+  //   fetch('http://localhost:3000/api/blogs').then((a)=>{
+  //     return a.json()
+  //   }).then((parsed)=>{
+  //     console.log(parsed)
+  //     setBlogs(parsed)
+  //   })
+  // }, [])
 
   return (
     <div className={styles.container}>
@@ -31,6 +32,14 @@ const Blog = () => {
         </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  let data = await fetch('http://localhost:3000/api/blogs')
+  let allBlogs = await data.json();
+  return {
+    props: {allBlogs}, // will be passed to the page component as props
+  }
 }
 
 export default Blog
